1e57bd6711897a8d8678700b03a72953d7f971ec
Zachary Taylor <zacharytaylor@Zacharys-MacBook-Pro.local>
2024-12-23 18:52:00 -0500

    Update users route

--- routes/users.js
+++ routes/users.js
@@ -8,35 +8,35 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
  
-router.post("/generateToken", (req, res) => {
+router.post("/generateToken", (req, res) => { //* 1
   
-  let jwtSecretKey = process.env.JWT_SECRET_KEY;
-  let data = {time: Date(),userId: 12,}
+  let jwtSecretKey = process.env.JWT_SECRET_KEY; //* 2
+  let data = {time: Date(),userId: 12,} //* 3
   
-  const token = jwt.sign(data, jwtSecretKey);
+  const token = jwt.sign(data, jwtSecretKey);//* 4
   
-  res.send(token);
+  res.send(token); //* 5
 }); 
 
-router.get("/validateToken", (req, res) => {
+router.get("/validateToken", (req, res) => { //* 6
     
   
-   let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
-   let jwtSecretKey = process.env.JWT_SECRET_KEY;
+   let tokenHeaderKey = process.env.TOKEN_HEADER_KEY; //* 7
+   let jwtSecretKey = process.env.JWT_SECRET_KEY; //* 8
   
     try {
-       const token = req.header(tokenHeaderKey);
+       const token = req.header(tokenHeaderKey); //* 9
   
-         const verified = jwt.verify(token, jwtSecretKey);
-        if(verified){
-            return res.send("Successfully Verified");
-         }else{
+         const verified = jwt.verify(token, jwtSecretKey); //* 10
+        if(verified){ //* 11
+            return res.send("Successfully Verified"); //* 11
+         }else{ //* 11
             // Access Denied
-             return res.status(401).send(error);
+             return res.status(401).send(error); //* 11
         }
-     } catch (error) {
+     } catch (error) { //* 12
         // Access Denied
-         return res.status(401).send(error);
+         return res.status(401).send(error); //* 12
     }
 });
 
