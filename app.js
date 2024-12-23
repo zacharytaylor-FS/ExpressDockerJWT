42dd15063b3a2f5baf6d524b8074cad009b57c8e
Zachary Taylor <zacharytaylor@Zacharys-MacBook-Pro.local>
2024-12-23 15:45:11 -0500

    Update user route

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/user', usersRouter);
 app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+ 
+router.post("/generateToken", (req, res) => {
+  
+  let jwtSecretKey = process.env.JWT_SECRET_KEY;
+  let data = {time: Date(),userId: 12,}
+  
+  const token = jwt.sign(data, jwtSecretKey);
+  
+  res.send(token);
+}); 
+
+router.get("/validateToken", (req, res) => {
+    
+  
+   let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+   let jwtSecretKey = process.env.JWT_SECRET_KEY;
+  
+    try {
+       const token = req.header(tokenHeaderKey);
+  
+         const verified = jwt.verify(token, jwtSecretKey);
+        if(verified){
+            return res.send("Successfully Verified");
+         }else{
+            // Access Denied
+             return res.status(401).send(error);
+        }
+     } catch (error) {
+        // Access Denied
+         return res.status(401).send(error);
+    }
 });
 
 module.exports = router;
